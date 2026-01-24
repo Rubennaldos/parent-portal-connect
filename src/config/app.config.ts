4f208a1c2dae9f0aecef15f018c34a2f876e8ff8
Rubennaldos <albertonaldos@gmail.com>
2026-01-23 22:58:05 -0500

    fix: Corregir versiÃ³n a v1.2.9

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
-  version: '1.4.0',
+  version: '1.2.9',
   status: 'BETA',
   appName: 'Lima Café 28',
   designedBy: 'ARQUISIA Soluciones',
-  fullVersion: 'v1.4.0-beta',
+  fullVersion: 'v1.2.9-beta',
 };
 
