52943670e77bc12296e4a5b270cfcb68f95e2e24
Rubennaldos <albertonaldos@gmail.com>
2026-01-27 15:42:58 -0500

    v1.3.7 - Fix flujo registro responsables (UPSERT)

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
-  version: '1.3.6',
+  version: '1.3.7',
   status: 'PRODUCTION',
   appName: 'Lima Café 28',
   designedBy: 'ARQUISIA Soluciones',
-  fullVersion: 'v1.3.6',
+  fullVersion: 'v1.3.7',
 };
 
