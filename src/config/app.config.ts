87a3823dbf4b38ba0554a314e00e038ad469dfc5
Rubennaldos <albertonaldos@gmail.com>
2026-01-31 12:03:46 -0500

    chore: bump version to 1.5.1

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
-  version: '1.5.0',
+  version: '1.5.1',
   status: 'PRODUCTION',
   appName: 'Lima Café 28',
   designedBy: 'ARQUISIA Soluciones',
-  fullVersion: 'v1.5.0',
+  fullVersion: 'v1.5.1',
 };
 
