9e10ebe87709196dd876ca4e85e5da2bdd3c22da
Rubennaldos <albertonaldos@gmail.com>
2026-02-03 09:03:50 -0500

    chore: Actualizar version visible a 1.10.0

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
-  version: '1.5.2',
+  version: '1.10.0',
   status: 'PRODUCTION',
   appName: 'Lima Café 28',
   designedBy: 'ARQUISIA Soluciones',
-  fullVersion: 'v1.5.2',
+  fullVersion: 'v1.10.0',
 };
 
